53098a54423208a071bea7e70586bef7a3eaa540
da <deniz@ayrota.com>
2025-01-16 09:48:50 +0300

    libc.so.6 fix for mac

--- src/dataloader/memmapped_sequences_dataset.py
+++ src/dataloader/memmapped_sequences_dataset.py
@@ -21,6 +21,7 @@ from .constants import *
 
 log = get_logger(__name__)
 
+
 class MemMappedSequencesDataset(Dataset, SequencesDataset):
     """
     This class contains a list of open numpy mem-mapped files,
@@ -49,8 +50,12 @@ class MemMappedSequencesDataset(Dataset, SequencesDataset):
             verbose=verbose,
         )
 
-        self.use_index_map = use_index_map # If false, calculate indices on the fly to save mem
-        self.keep_all_memmap_open = store_in_ram or keep_all_memmap_open # If true, keep all memmap files open the whole time
+        self.use_index_map = (
+            use_index_map  # If false, calculate indices on the fly to save mem
+        )
+        self.keep_all_memmap_open = (
+            store_in_ram or keep_all_memmap_open
+        )  # If true, keep all memmap files open the whole time
         self.store_in_ram = store_in_ram
 
         # Index the mem-mapped files (data is not read from disk here)
@@ -60,34 +65,45 @@ class MemMappedSequencesDataset(Dataset, SequencesDataset):
             # We found that this can be faster and more mem-efficient with a large dataset.
             self.setup_on_the_fly_indexing()
 
-        self.load_memmap_files()   
-        
+        self.load_memmap_files()
+
     def build_index_map(self):
-        # The index map will map to the indices in the base sensor, which is the 
+        # The index map will map to the indices in the base sensor, which is the
         # first one in the sensor_file_basenames list (IMU0 typically)
         # If we need to load data from other sensor files (i.e, if data_style != "combined")
         # we will perform a guided search for the timestamps in the other files,
         # with the help of the "approximate_frequency" attribute in the json files.
-        
+
         base_sensor_name = self.get_base_sensor_name()
 
-        self.index_map = np.empty((sum([
-            d[base_sensor_name]["num_rows"]-self.genparams.window_size+1 for d in self.data_descriptions
-        ]), 2), dtype=np.int32)
+        self.index_map = np.empty(
+            (
+                sum(
+                    [
+                        d[base_sensor_name]["num_rows"] - self.genparams.window_size + 1
+                        for d in self.data_descriptions
+                    ]
+                ),
+                2,
+            ),
+            dtype=np.int32,
+        )
         curr_idx = 0
 
         for i, desc in enumerate(self.data_descriptions):
             rows = desc[base_sensor_name]["num_rows"]
             idx_rows = rows - self.genparams.window_size + 1
             # Sequence index
-            self.index_map[curr_idx:curr_idx+idx_rows,0] = i
+            self.index_map[curr_idx : curr_idx + idx_rows, 0] = i
             # Index within the sequence
-            self.index_map[curr_idx:curr_idx+idx_rows,1] = np.arange(idx_rows, dtype=np.int32)
+            self.index_map[curr_idx : curr_idx + idx_rows, 1] = np.arange(
+                idx_rows, dtype=np.int32
+            )
             curr_idx += idx_rows
         assert curr_idx == self.index_map.shape[0]
 
         # Decimate the data to avoid redundant samples
-        self.index_map = self.index_map[::self.genparams.decimator]
+        self.index_map = self.index_map[:: self.genparams.decimator]
         self.length = len(self.index_map)
 
         """ Attempt more mem efficient index map (not just take ::step at the end, build it smaller at first)
@@ -117,17 +133,18 @@ class MemMappedSequencesDataset(Dataset, SequencesDataset):
         # search on this array to find the index in the sequence for the timestamp.
         # We found in preliminary tests that this was faster and more memory efficient
         # for scaled-up training.
-        
+
         # For each sequence, the total data length up to that point
-        data_dim = np.array([
-            d[self.get_base_sensor_name()]["num_rows"] for d in self.data_descriptions
-        ])
-        self.list_cumsum = np.cumsum((
-            data_dim - self.genparams.window_size #+ 1
-        ) // self.genparams.decimator, dtype=np.int64)
-        #self.length = (
+        data_dim = np.array(
+            [d[self.get_base_sensor_name()]["num_rows"] for d in self.data_descriptions]
+        )
+        self.list_cumsum = np.cumsum(
+            (data_dim - self.genparams.window_size) // self.genparams.decimator,  # + 1
+            dtype=np.int64,
+        )
+        # self.length = (
         #    np.sum(self.data_dim) + len(self.data_list)*(1-self.genparams.window_size)
-        #) // self.genparams.decimator
+        # ) // self.genparams.decimator
         self.length = self.list_cumsum[-1] if len(self.list_cumsum) > 0 else 0
 
     def load_memmap_files(self):
@@ -145,27 +162,43 @@ class MemMappedSequencesDataset(Dataset, SequencesDataset):
             seq_memmap_filenames = {}
             desc = self.data_descriptions[i]
             for j, sensor_basename in enumerate(self.sensor_file_basenames):
-                filename = os.path.join(self.data_path, seq_id, sensor_basename+".npy")
+                filename = os.path.join(
+                    self.data_path, seq_id, sensor_basename + ".npy"
+                )
                 seq_memmap_filenames[sensor_basename] = filename
                 sensor_desc = desc[sensor_basename]
-                num_cols = sum([
-                    int(c.split("(")[1].split(")")[0]) for c in sensor_desc["columns_name(width)"]
-                ])
-                cumulated_duration_hrs += 1e-6 * (sensor_desc["t_end_us"] - sensor_desc["t_start_us"]) / 60 / 60
-                self.max_num_rows = sensor_desc["num_rows"] if self.max_num_rows is None \
-                        else max(sensor_desc["num_rows"], self.max_num_rows)
-                self.min_num_rows = sensor_desc["num_rows"] if self.min_num_rows is None \
-                        else min(sensor_desc["num_rows"], self.min_num_rows)
+                num_cols = sum(
+                    [
+                        int(c.split("(")[1].split(")")[0])
+                        for c in sensor_desc["columns_name(width)"]
+                    ]
+                )
+                cumulated_duration_hrs += (
+                    1e-6
+                    * (sensor_desc["t_end_us"] - sensor_desc["t_start_us"])
+                    / 60
+                    / 60
+                )
+                self.max_num_rows = (
+                    sensor_desc["num_rows"]
+                    if self.max_num_rows is None
+                    else max(sensor_desc["num_rows"], self.max_num_rows)
+                )
+                self.min_num_rows = (
+                    sensor_desc["num_rows"]
+                    if self.min_num_rows is None
+                    else min(sensor_desc["num_rows"], self.min_num_rows)
+                )
                 if self.keep_all_memmap_open:
                     if self.store_in_ram:
                         fp = np.load(filename)
                     else:
-                        fp = np.load(filename, mmap_mode='c')
+                        fp = np.load(filename, mmap_mode="c")
 
                     seq_fps[sensor_basename] = fp
             self.fps[i] = seq_fps
-            self.memmap_filenames[i] = seq_memmap_filenames 
-    
+            self.memmap_filenames[i] = seq_memmap_filenames
+
     def map_index(self, idx):
         if self.use_index_map:
             return self.index_map[idx]
@@ -176,27 +209,29 @@ class MemMappedSequencesDataset(Dataset, SequencesDataset):
         """
         Give the index of the sequence and row in sequence without storing large flat index map
         """
-        random_idx = False #self.split=="train" and len(self.list_cumsum) > 3000
-        
+        random_idx = False  # self.split=="train" and len(self.list_cumsum) > 3000
+
         if not random_idx:
             # binsearch is super slow, try to speed it up with some bounds
             # Minimum sequence loc based on min num rows (if all sequences were min_num_rows long)
-            #seq_idx_max = idx // self.min_num_rows
-            #seq_idx_min = idx // self.max_num_rows
-            #seq_idx = np.searchsorted(self.list_cumsum[seq_idx_min:seq_idx_max+1], idx) + seq_idx_min
+            # seq_idx_max = idx // self.min_num_rows
+            # seq_idx_min = idx // self.max_num_rows
+            # seq_idx = np.searchsorted(self.list_cumsum[seq_idx_min:seq_idx_max+1], idx) + seq_idx_min
             seq_idx = np.searchsorted(self.list_cumsum, idx)
-            #print(idx, seq_idx_min, seq_idx_max, seq_idx, seq_idx_check)
-            #assert seq_idx == seq_idx_check
+            # print(idx, seq_idx_min, seq_idx_max, seq_idx, seq_idx_check)
+            # assert seq_idx == seq_idx_check
 
             assert seq_idx < len(self.list_cumsum)
             row = (
-                idx - (self.list_cumsum[seq_idx-1] if seq_idx>0 else 0)
+                idx - (self.list_cumsum[seq_idx - 1] if seq_idx > 0 else 0)
             ) * self.genparams.decimator
-            max_rows = self.data_descriptions[seq_idx][self.get_base_sensor_name()]["num_rows"]
-            assert row < max_rows - self.genparams.window_size + 1 
+            max_rows = self.data_descriptions[seq_idx][self.get_base_sensor_name()][
+                "num_rows"
+            ]
+            assert row < max_rows - self.genparams.window_size + 1
         else:
             seq_idx = np.random.randint(len(self.list_cumsum))
-            
+
             """
             # Each worker only responsible for chunk of dataset
             winfo = torch.utils.data.get_worker_info()
@@ -205,28 +240,33 @@ class MemMappedSequencesDataset(Dataset, SequencesDataset):
                 (1+winfo.id)*len(self.list_cumsum)//winfo.num_workers,
             )"""
 
-            row = np.random.randint(self.list_cumsum[seq_idx] - (self.list_cumsum[seq_idx-1] if seq_idx!=0 else 0))
+            row = np.random.randint(
+                self.list_cumsum[seq_idx]
+                - (self.list_cumsum[seq_idx - 1] if seq_idx != 0 else 0)
+            )
 
         return seq_idx, row
-    
+
     def __len__(self):
         return self.length
 
     def __getitem__(self, idx):
         seq_idx, row_in_seq = self.map_index(idx)
         ret = self.load_and_preprocess_data_chunk(
-            seq_idx, row_in_seq, 
-            self.data_descriptions[seq_idx][self.get_base_sensor_name()]["num_rows"]-self.genparams.window_size,
+            seq_idx,
+            row_in_seq,
+            self.data_descriptions[seq_idx][self.get_base_sensor_name()]["num_rows"]
+            - self.genparams.window_size,
         )
         return ret
 
-    def load_data_chunk(self, seq_idx, row):       
-        if not hasattr(self, "madvise"):
-            # https://github.com/numpy/numpy/issues/13172
-            # Doesn't seem to help our speed
-            self.madvise = ctypes.CDLL("libc.so.6").madvise
-            self.madvise.argtypes = [ctypes.c_void_p, ctypes.c_size_t, ctypes.c_double]
-            self.madvise.restype = ctypes.c_double
+    def load_data_chunk(self, seq_idx, row):
+        # if not hasattr(self, "madvise"):
+        #     # https://github.com/numpy/numpy/issues/13172
+        #     # Doesn't seem to help our speed
+        #     self.madvise = ctypes.CDLL("libc.so.6").madvise
+        #     self.madvise.argtypes = [ctypes.c_void_p, ctypes.c_size_t, ctypes.c_double]
+        #     self.madvise.restype = ctypes.c_double
 
         seq_desc = self.data_descriptions[seq_idx]
         if self.keep_all_memmap_open:
@@ -235,14 +275,19 @@ class MemMappedSequencesDataset(Dataset, SequencesDataset):
             seq_data = {}
             for sensor_name, sensor_desc in seq_desc.items():
                 filename = self.memmap_filenames[seq_idx][sensor_name]
-                num_cols = sum([
-                    int(c.split("(")[1].split(")")[0]) for c in sensor_desc["columns_name(width)"]
-                ])
-                seq_data[sensor_name] = np.load(filename, mmap_mode='c') #np.memmap(filename, dtype=np.float64, mode='c',
+                num_cols = sum(
+                    [
+                        int(c.split("(")[1].split(")")[0])
+                        for c in sensor_desc["columns_name(width)"]
+                    ]
+                )
+                seq_data[sensor_name] = np.load(
+                    filename, mmap_mode="c"
+                )  # np.memmap(filename, dtype=np.float64, mode='c',
                 #        shape=(sensor_desc["num_rows"],num_cols))
-    
+
         return self.data_chunk_from_seq_data(seq_data, seq_desc, row)
-    
+
     def get_ts_last_imu_us(self, seq_idx=0):
         """
         Get the last timestamp for all IMU windows in this sequence
@@ -255,23 +300,29 @@ class MemMappedSequencesDataset(Dataset, SequencesDataset):
         else:
             desc = self.data_descriptions[seq_idx][self.get_base_sensor_name()]
             filename = self.memmap_filenames[seq_idx][self.get_base_sensor_name()]
-            num_cols = sum([
-                int(c.split("(")[1].split(")")[0]) for c in desc["columns_name(width)"]
-            ])
-            #fp = np.memmap(filename, dtype=np.float64, mode='c', shape=(desc["num_rows"],num_cols))
-            fp = np.load(filename, mmap_mode='c')
-        ts = fp[self.genparams.window_size-1::self.genparams.decimator,0]
-        if not self.use_index_map: # off-by-one sometimes between these two options
-            #new_len = (len(fp) - self.genparams.window_size + 1) // self.genparams.decimator
-            new_len = self.list_cumsum[seq_idx] - (self.list_cumsum[seq_idx-1] if seq_idx!=0 else 0)
-            assert abs(new_len - len(ts)) < 2, \
-                    f"Expected off-by-one at most, but got {abs(new_len - len(ts))}"
+            num_cols = sum(
+                [
+                    int(c.split("(")[1].split(")")[0])
+                    for c in desc["columns_name(width)"]
+                ]
+            )
+            # fp = np.memmap(filename, dtype=np.float64, mode='c', shape=(desc["num_rows"],num_cols))
+            fp = np.load(filename, mmap_mode="c")
+        ts = fp[self.genparams.window_size - 1 :: self.genparams.decimator, 0]
+        if not self.use_index_map:  # off-by-one sometimes between these two options
+            # new_len = (len(fp) - self.genparams.window_size + 1) // self.genparams.decimator
+            new_len = self.list_cumsum[seq_idx] - (
+                self.list_cumsum[seq_idx - 1] if seq_idx != 0 else 0
+            )
+            assert (
+                abs(new_len - len(ts)) < 2
+            ), f"Expected off-by-one at most, but got {abs(new_len - len(ts))}"
             ts = ts[:new_len]
         return ts
 
     def get_gt_traj_center_window_times(self, seq_idx=0):
         """
-        Get the GT orientatoin/position (in world frame) at the center 
+        Get the GT orientatoin/position (in world frame) at the center
         time for each IMU window in this sequence.
         Returned as stacked [N,4,4] SE3 matrices
         """
@@ -283,19 +334,27 @@ class MemMappedSequencesDataset(Dataset, SequencesDataset):
         else:
             desc = self.data_descriptions[seq_idx][self.get_base_sensor_name()]
             filename = self.memmap_filenames[seq_idx][self.get_base_sensor_name()]
-            num_cols = sum([
-                int(c.split("(")[1].split(")")[0]) for c in desc["columns_name(width)"]
-            ])
-            #fp = np.memmap(filename, dtype=np.float64, mode='c', shape=(desc["num_rows"],num_cols))
-            fp = np.load(filename, mmap_mode='c')
+            num_cols = sum(
+                [
+                    int(c.split("(")[1].split(")")[0])
+                    for c in desc["columns_name(width)"]
+                ]
+            )
+            # fp = np.memmap(filename, dtype=np.float64, mode='c', shape=(desc["num_rows"],num_cols))
+            fp = np.load(filename, mmap_mode="c")
 
         start_idx = (self.genparams.window_size - 1) // 2
-        end = len(fp) - self.genparams.window_size//2
-        traj = fp[start_idx:end:self.genparams.decimator,-10:-3] # [start:stop:step]
-        if not self.use_index_map: # off-by-one sometimes between these two options
-            #new_len = (len(fp) - self.genparams.window_size + 1) // self.genparams.decimator
-            new_len = self.list_cumsum[seq_idx] - (self.list_cumsum[seq_idx-1] if seq_idx!=0 else 0)
-            assert abs(new_len - len(traj)) < 2, \
-                    f"Expected off-by-one at most, but got {abs(new_len - len(traj))}"
+        end = len(fp) - self.genparams.window_size // 2
+        traj = fp[
+            start_idx : end : self.genparams.decimator, -10:-3
+        ]  # [start:stop:step]
+        if not self.use_index_map:  # off-by-one sometimes between these two options
+            # new_len = (len(fp) - self.genparams.window_size + 1) // self.genparams.decimator
+            new_len = self.list_cumsum[seq_idx] - (
+                self.list_cumsum[seq_idx - 1] if seq_idx != 0 else 0
+            )
+            assert (
+                abs(new_len - len(traj)) < 2
+            ), f"Expected off-by-one at most, but got {abs(new_len - len(traj))}"
             traj = traj[:new_len]
-        return Rotation.from_quat(traj[:,:4]), traj[:,4:]
+        return Rotation.from_quat(traj[:, :4]), traj[:, 4:]
